c4a03c0ef27eecadc176562e93f47e12110dca5e
Lilahamstern <leo.ronnebro@gmail.com>
2019-11-02 20:19:34 +0100

    Added docker build and service

--- project-service/build.sh
+++ project-service/build.sh
@@ -1,3 +1,7 @@
 set GOOS=linux
 go build -o projectservice-linux-amd64
 set GOOS=windows
+
+docker build -t bec/projectservice .
+
+docker service create --name=projectservice --replicas=1 --network=bec_overlay -p=5050:5050 bec/projectservice
