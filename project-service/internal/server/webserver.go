3dbe0d7fbcbb5acfb41cf661243449c1f79afa19
lilahamstern <leo.ronnebro@gmail.com>
2019-11-06 22:27:47 +0100

    Typo

--- project-service/internal/server/webserver.go
+++ project-service/internal/server/webserver.go
@@ -9,7 +9,7 @@ func StartWebService(port string, engine *gin.Engine) {
 	log.Println("Starting HTTP server at " + port)
 	err := engine.Run(":" + port)
 	if err != nil {
-		log.Println("An error occured starting HTTP listener at port " + port)
+		log.Println("An error occurred starting HTTP listener at port " + port)
 		log.Println("Error: " + err.Error())
 	}
 }
