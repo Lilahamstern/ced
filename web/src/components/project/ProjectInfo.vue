3173d9a0ea61decd3c49fa96c054fd06e8299179
Lilahamstern <leo.ronnebro@gmail.com>
2019-12-03 14:34:00 +0100

    Small changes to API

--- web/src/components/project/ProjectInfo.vue
+++ web/src/components/project/ProjectInfo.vue
@@ -1,8 +1,16 @@
 <template>
   <div>
-    <b-card bg-variant="light" class="text-dark" v-if="getProjectInfo" no-body>
+    <b-card
+      bg-variant="light"
+      class="text-dark"
+      v-if="getProjectInfo"
+      no-body
+    >
       <b-card-header header-tag="nav">
-        <b-nav card-header tabs>
+        <b-nav
+          card-header
+          tabs
+        >
           <b-nav-item active>Ekonomi</b-nav-item>
           <b-nav-item>Nagot</b-nav-item>
           <b-nav-item>Leverans</b-nav-item>
@@ -10,20 +18,24 @@
       </b-card-header>
 
       <b-card-body class="text-center">
-        <b-card-sub-title :sub-title="getProjectInfo.name" sub-title-tag="h4" class="my-2">
+        <b-card-sub-title
+          :sub-title="getProjectInfo.name"
+          sub-title-tag="h4"
+          class="my-2"
+        >
         </b-card-sub-title>
         <b-card-text>
-          <p>{{ getProjectInfo.desc }}</p>
+          <p>{{ projectInfo.desc }}</p>
         </b-card-text>
         <hr class="my-2" />
         <div class="text-left">
-          <span>Client: {{ getProjectInfo.client }}</span>
+          <span>Client: {{ projectInfo.client }}</span>
           <hr class="my-2" />
-          <span>Sector: {{ getProjectInfo.sector }}</span>
+          <span>Sector: {{ projectInfo.sector }}</span>
           <hr class="my-2" />
-          <span>Co2/ekv: {{ getProjectInfo.co2 }}</span>
+          <span>Co2/ekv: {{ projectInfo.id }}</span>
           <hr class="my-2" />
-          <span>Status: {{ getProjectInfo.state }}</span>
+          <span>Status: {{ projectInfo.state }}</span>
           <hr class="my-2" />
           <b-button>More details</b-button>
         </div>
@@ -45,10 +57,11 @@ export default class ProjectInfo extends Vue {
   @Getter("projectInfo", { namespace }) projectInfo!: Project;
 
   get getProjectInfo() {
-    console.log(this.projectInfo)
+    console.log(this.projectInfo);
     if (!this.projectInfo) {
       return undefined;
     }
+    console.log(this.projectInfo.client);
     return this.projectInfo;
   }
 }
--- web/src/store/project/actions.ts
+++ web/src/store/project/actions.ts
@@ -4,22 +4,23 @@ import { RootState } from '../types';
 import axios from 'axios';
 
 export const actions: ActionTree<ProjectState, RootState> = {
-  fetchProjects ({ commit }): any {
-    axios({
-      url: "http://localhost:5050/projects"
-    }).then(res => {
+  async fetchProjects ({ commit }) {
+    try {
+      var res = await axios.get("http://localhost:5050/projects");
       const payload: Project = res && res.data.data
       console.log(res.status)
       if (res.status != 200) {
         console.log("heyyy")
       }
       commit("projectLoaded", payload)
-    }).catch(error => {
-      console.log(error)
-      commit("projectError")
-    })
+    } catch (err) {
+      console.error("Error response:");
+      console.error(err.response.data);    // ***
+      console.error(err.response.status);  // ***
+      console.error(err.response.headers); // ***
+    }
   },
-  selectProject ({ commit, state}, payload: any): any {
+  selectProject ({ commit, state }, payload: any): any {
     if (!state.projects) return
     commit("selectProject", state.projects[payload.index])
   }
