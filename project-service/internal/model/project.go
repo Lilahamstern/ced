9c14df16dd928728fa6f5f8b7c8bac89dcaec210
lilahamstern <leo.ronnebro@gmail.com>
2019-12-03 21:27:23 +0100

    Added state to database

--- project-service/internal/model/project.go
+++ project-service/internal/model/project.go
@@ -1,12 +1,13 @@
 package model
 
 type Project struct {
-	Id    string `json:"id"`
+	Id     string `json:"id"`
 	Client string `json:"client"`
 	Sector string `json:"sector"`
-	Name  string `json:"name"`
-	Model string `json:"model"`
-	Desc  string `json:"desc"`
+	Name   string `json:"name"`
+	Model  string `json:"model"`
+	Desc   string `json:"desc"`
+	State  string `json:"state"`
 }
 
 type Projects []Project
