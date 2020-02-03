e5695d3f3f98a3322808bb5714c5586c24ecc6df
Lilahamstern <leo.ronnebro@gmail.com>
2020-02-03 23:26:30 +0100

    Renamed namespace

--- Api/Api/Contracts/V1/Responses/ProjectResponse.cs
+++ Api/Api/Contracts/V1/Responses/ProjectResponse.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Api.Controllers.V1.Responses
+namespace Api.Contracts.V1.Responses
 {
     public class ProjectResponse
     {
