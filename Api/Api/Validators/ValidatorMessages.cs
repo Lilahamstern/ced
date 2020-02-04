632fbb388cebbceea2416731b5b40a191e5b9423
Lilahamstern <leo.ronnebro@gmail.com>
2020-02-04 23:04:49 +0100

    Added Validator messages and update validation file

---
+++ Api/Api/Validators/ValidatorMessages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Validators
+{
+    public static class ValidatorMessages
+    {
+        public const string PropertyRequierd = "{PropertyName} is requierd";
+        public const string MaxLength = "Maximum length of {PropertyName} is {MaxLength} characters";
+        public const string NotNull = "{PropertyName} cannot be null";
+        public const string OnlyStringAndNumbers = "{PropertyName} can only contain letter and numbers";
+    }
+}
