6dd1c8a5583371a6a60cbccd595bda2bb39fdd07
Lilahamstern <leo.ronnebro@gmail.com>
2020-02-23 20:35:09 +0100

    Added installer to GRPC server and GRPC helper to client

---
+++ Server.gRPC/Installers/GRPCInstaller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.gRPC.Installers
+{
+    public class GRPCInstaller : IInstaller
+    {
+        public void InstallServices(IConfiguration configuration, IServiceCollection services)
+        {
+            services.AddGrpc(o => o.EnableDetailedErrors = true);
+        }
+    }
+}
--- Server.gRPC/Startup.cs
+++ Server.gRPC/Startup.cs
@@ -1,18 +1,27 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Server.gRPC.Installers;
+using System.Configuration;
 
 namespace Server.gRPC
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddGrpc();
+            services.InstallServicesInAssembly(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
