da1357ea9c59e3554362b2e3a5a0d7f659a42644
Leo Rönnebro <leo.ronnebro@gmail.com>
2020-03-03 10:59:08 +0100

    Removed and swaped database to MYSQL

--- Database/dbo/Tables/Projects.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Projects] (
-    [Id]        INT           NOT NULL,
-    [CreatedAt] DATETIME2 (7) NULL,
-    [UpdatedAt] DATETIME2 (7) NULL,
-    CONSTRAINT [PK_Projects] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
