a2ed4b17733005093e4792e09c36a8f761545c82
Leo Rönnebro <leo.ronnebro@gmail.com>
2020-03-09 20:37:11 +0100

    Updated table design and working on nested rows on select

--- web/src/components/projects/Table.vue
+++ web/src/components/projects/Table.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="bg-green-800">
-    <p v-if="!loading">
+  <div class="mt-16 flex flex-wrap content-around">
+    <p v-if="!loading" class="mb-4 w-full text-left">
       {{ $t('table.show') }}
       <select
         v-model="pageSize"
@@ -11,98 +11,110 @@
           v-for="option in options"
           :value="option.value"
           :key="option.value"
-          >{{ option.value }}</option
-        >
+        >{{ option.value }}</option>
       </select>
     </p>
     <p v-if="loading" class="mx-auto">Loading</p>
-    <table class="table-auto mx-auto" v-if="!loading">
+    <table
+      class="border-collapse mx-auto border-collapse border-2 border-gray-600 w-full text-left"
+      v-if="!loading"
+    >
       <thead>
         <tr>
-          <th class="px-4 py-2" @click="sort('orderId')">
-            {{ $t('project.orderId') }}
-          </th>
-          <th class="px-4 py-2" @click="sort('name')">
-            {{ $t('project.name') }}
-          </th>
-          <th class="px-4 py-2" @click="sort('manager')">
-            {{ $t('project.manager') }}
-          </th>
-          <th class="px-4 py-2" @click="sort('client')">
-            {{ $t('project.client') }}
-          </th>
-          <th class="px-4 py-2" @click="sort('sector')">
-            {{ $t('project.sector') }}
-          </th>
-          <th class="px-4 py-2" @click="sort('projectId')">
-            {{ $t('project.projectId') }}
-          </th>
+          <th class="px-4 py-2" @click="sort('orderId')">{{ $t('project.orderId') }}</th>
+          <th class="px-4 py-2" @click="sort('name')">{{ $t('project.name') }}</th>
+          <th class="px-4 py-2" @click="sort('manager')">{{ $t('project.manager') }}</th>
+          <th class="px-4 py-2" @click="sort('client')">{{ $t('project.client') }}</th>
+          <th class="px-4 py-2" @click="sort('sector')">{{ $t('project.sector') }}</th>
+          <th class="px-4 py-2" @click="sort('projectId')">{{ $t('project.projectId') }}</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="project in sortedProjects" :key="project.id">
-          <td class="border px-4 py-2">{{ project.orderId }}</td>
-          <td class="border px-4 py-2">{{ project.name }}</td>
-          <td class="border px-4 py-2">{{ project.manager }}</td>
-          <td class="border px-4 py-2">{{ project.client }}</td>
-          <td class="border px-4 py-2">{{ project.sector }}</td>
-          <td class="border px-4 py-2">{{ project.projectId }}</td>
-        </tr>
+        <template v-for="project in sortedProjects">
+          <tr @click="extend(project.projectId)" :key="project.projectId" class="text-left">
+            <td class="border-b border-gray-500 px-4 py-2 cursor-pointer w-32">{{ project.orderId }}</td>
+            <td class="border-b border-gray-500 px-4 py-2 cursor-pointer w-40">{{ project.name }}</td>
+            <td class="border-b border-gray-500 px-4 py-2 cursor-pointer w-64">{{ project.manager }}</td>
+            <td class="border-b border-gray-500 px-4 py-2 cursor-pointer w-40">{{ project.client }}</td>
+            <td
+              class="border-b border-gray-500 px-4 py-2 cursor-pointer w-64 truncate"
+            >{{ project.sector }}</td>
+            <td
+              class="border-b border-gray-500 px-4 py-2 cursor-pointer w-30"
+            >{{ project.projectId }}</td>
+          </tr>
+          <!-- <tr
+            v-if="selectedProject == project.projectId"
+            :key="project.projectId*2000"
+            class="bg-gray-500 half-width cursor-pointer"
+          >
+            <td class="border-b border-gray-500 cursor-pointer"></td>
+            <td class="border-b border-gray-500 px-4 py-2 cursor-pointer">{{ project.name }}</td>
+            <td class="border-b border-gray-500 px-4 py-2 cursor-pointer">{{ project.manager }}</td>
+            <td class="border-b border-gray-500 px-4 py-2 cursor-pointer">{{ project.client }}</td>
+            <td class="border-b border-gray-500 px-4 py-2 cursor-pointer">{{ project.sector }}</td>
+            <td class="border-b border-gray-500 px-4 py-2 cursor-pointer">{{ project.projectId }}</td>
+          </tr>-->
+        </template>
       </tbody>
     </table>
-    <div class="inline-flex mt-4 h-8" v-if="!loading">
-      <button
-        class="bg-gray-300 hover:bg-gray-400 text-gray-800 py-0 px-2"
-        @click="prevPage"
-      >
-        {{ $t('table.prev') }}
-      </button>
-      <button
-        class="bg-gray-300 hover:bg-gray-400 text-gray-800 py-0 px-2"
-        @click="nextPage"
-      >
-        {{ $t('table.next') }}
-      </button>
+    <div class="inline-flex mt-4 h-8 w-full" v-if="!loading">
+      <div class="w-1/2 text-left h-full">
+        <button
+          class="bg-gray-300 hover:bg-gray-400 text-gray-800 py-0 mr-4 px-2 h-full"
+          @click="prevPage"
+        >{{ $t('table.prev') }}</button>
+        <button
+          class="bg-gray-300 hover:bg-gray-400 text-gray-800 py-0 px-2 h-full"
+          @click="nextPage"
+        >{{ $t('table.next') }}</button>
+      </div>
 
-      <p class=" px-2 my-auto">
-        {{ $t('table.showing', [showingProjects, projects.length]) }}
-      </p>
+      <p
+        class="w-1/2 text-right h-full"
+      >{{ $t('table.showing', [showingProjects, projects.length]) }}</p>
     </div>
   </div>
 </template>
 
 <script>
-import projectGRPC from '../../grpc/project/projectClient';
+import projectGRPC from "../../grpc/project/projectClient";
 export default {
-  name: 'project-table',
+  name: "project-table",
   data: function() {
     return {
+      selectedProject: null,
       projects: [],
-      currentSort: 'asc',
-      currentSortDir: 'name',
+      currentSort: "asc",
+      currentSortDir: "name",
       loading: true,
       pageSize: 10,
       currentPage: 1,
-      options: [{ value: 5 }, { value: 10 }, { value: 20 }, { value: 25 }]
+      options: [{ value: 5 }, { value: 10 }, { value: 15 }, { value: 20 }]
     };
   },
   methods: {
     sort: function(sort) {
       if (sort === this.currentSort) {
-        this.currentSortDir = this.currentSortDir === 'asc' ? 'desc' : 'asc';
+        this.currentSortDir = this.currentSortDir === "asc" ? "desc" : "asc";
       }
       this.currentSort = sort;
     },
     nextPage: function() {
       if (this.currentPage * this.pageSize < this.projects.length)
         this.currentPage++;
+      this.selectedProject = 0;
     },
     prevPage: function() {
       if (this.currentPage > 1) this.currentPage--;
+      this.selectedProject = 0;
     },
     sizeValidation: function() {
       let currentSize = this.pageSize * this.currentPage;
       if (currentSize > this.projects.length) return (this.currentPage = 1);
+    },
+    extend: function(id) {
+      this.selectedProject = id;
     }
   },
   computed: {
@@ -111,7 +123,7 @@ export default {
         .slice(0)
         .sort((a, b) => {
           let modifier = 1;
-          if (this.currentSortDir === 'desc') modifier = -1;
+          if (this.currentSortDir === "desc") modifier = -1;
           if (a[this.currentSort] < b[this.currentSort]) return -1 * modifier;
           if (a[this.currentSort] > b[this.currentSort]) return 1 * modifier;
           return 0;
