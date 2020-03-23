bd963b90db7240bee8eeb82805c9d1e13d3349b2
Leo Rönnebro <leo.ronnebro@hamsterapps.net>
2020-03-23 22:01:44 +0100

    Added FontAwesome

--- web/src/main.js
+++ web/src/main.js
@@ -5,10 +5,15 @@ import store from './store';
 import '@/assets/css/tailwind.css';
 import devtools from '@vue/devtools';
 import i18n from './locales/i18n';
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 if (process.env.NODE_ENV === 'development') {
   devtools.connect();
 }
+
+library.add(fas);
+dom.watch();
 
 Vue.config.productionTip = true;
 Vue.config.performance = true;
