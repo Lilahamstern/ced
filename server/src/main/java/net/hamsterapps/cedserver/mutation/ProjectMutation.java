200993634d69ded62b949004c2c25e34d45e95c3
Leo Rönnebro <leo.ronnebro@hamsterapps.net>
2020-05-09 16:27:38 +0200

    Added compnent signature

--- server/src/main/java/net/hamsterapps/cedserver/mutation/ProjectMutation.java
+++ server/src/main/java/net/hamsterapps/cedserver/mutation/ProjectMutation.java
@@ -1,11 +1,13 @@
 package net.hamsterapps.cedserver.mutation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import graphql.kickstart.tools.GraphQLMutationResolver;
 import net.hamsterapps.cedserver.model.Project;
 import net.hamsterapps.cedserver.service.ProjectService;
 
+@Component
 public class ProjectMutation implements GraphQLMutationResolver {
 
   @Autowired
