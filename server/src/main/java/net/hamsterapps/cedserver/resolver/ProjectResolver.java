670e7cd89258ef93b5bbf365ef2030fa1daca86e
Lilahamstern <leo.ronnebro@hamsterapps.net>
2020-06-02 21:34:55 +0200

    Merge error

--- server/src/main/java/net/hamsterapps/cedserver/resolver/ProjectResolver.java
+++ server/src/main/java/net/hamsterapps/cedserver/resolver/ProjectResolver.java
@@ -1,5 +1,9 @@
 package net.hamsterapps.cedserver.resolver;
 
+import graphql.kickstart.tools.GraphQLResolver;
+import net.hamsterapps.cedserver.model.Project;
+import net.hamsterapps.cedserver.model.Version;
+import net.hamsterapps.cedserver.service.VersionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
