052aec84520217fbb8382b15b2c9d0eaeda6fb52
Lilahamstern <leo.ronnebro@hamsterapps.net>
2020-07-04 19:03:41 +0200

    Init of web-ui v2 with tailwindcss

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,33 +1,19 @@
 <template>
-  <div id="app">
-    <v-app color="primary">
-      <v-app-bar app>
-        <Navbar/>
-      </v-app-bar>
-      <v-main>
-        <v-container>
-          <router-view></router-view>
-        </v-container>
-      </v-main>
-    </v-app>
-  </div>
+    <div id="app" class="bg-gray-900 w-full h-screen">
+        <Nav/>
+        <div class="p-12">
+            <router-view/>
+        </div>
+    </div>
 </template>
 
-<script>
-import Navbar from "@/components/Navbar";
-export default {
-  name: 'App',
-  components: {Navbar},
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
+<script>
+    import Nav from "@/components/Nav";
+
+    export default {
+        components: {Nav}
+    }
+</script>
---
+++ web/src/views/Projects.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container mx-auto">
+        <ProjectTable></ProjectTable>
+    </div>
+</template>
+
+<script>
+
+    import ProjectTable from "@/components/projects/ProjectTable";
+    export default {
+        name: 'Home',
+        components: {ProjectTable},
+    }
+</script>
+
+<style type="text/css" scoped>
+
+</style>
