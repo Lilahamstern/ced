2626fb7eb27edbe3a0b56eb612eadc24cf6b2860
Lilahamstern <leo.ronnebro@hamsterapps.net>
2020-07-12 23:42:55 +0200

    Creation of project now works

--- server/internal/controller/project.go
+++ server/internal/controller/project.go
@@ -1,21 +1,44 @@
 package controller
 
 import (
+	"fmt"
 	"github.com/gofiber/fiber"
+	"github.com/lilahamstern/ced/server/pkg/errors"
 	"github.com/lilahamstern/ced/server/pkg/model"
 	"net/http"
 )
 
 func (s *Controller) CreateProject(c *fiber.Ctx) {
+	const op errors.Op = "controller.createProject"
 	var body model.CreateProject
 	// Wants to use custom validation for input data, so no error will be handled
 	_ = c.BodyParser(&body)
 
-	err := body.Validate()
-	if err != nil {
+	if err := body.Validate(); err != nil {
 		respondError(c, http.StatusBadRequest, err)
 		return
 	}
 
-	respondData(c, http.StatusOK, body)
+	exists, err := s.repos.Project.Exists(body.ID)
+	if err != nil {
+		s.log.SystemErr(err)
+		respondError(c, errors.Kind(err), "Internal Server Error")
+		return
+	}
+
+	if exists {
+		// TODO: Add exist validation to validator
+		msg := fmt.Sprintf("project already exists with id: %v", body.ID)
+		respondError(c, http.StatusConflict, msg)
+		return
+	}
+
+	project, err := s.repos.Project.Save(body)
+	if err != nil {
+		s.log.SystemErr(err)
+		respondError(c, errors.Kind(err), "Internal Server Error")
+		return
+	}
+
+	respondData(c, http.StatusOK, project)
 }
