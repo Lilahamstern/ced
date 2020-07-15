6fe0687c36a611226bd3b4e7bc1d4af5706b7ea8
Lilahamstern <leo.ronnebro@hamsterapps.net>
2020-07-16 00:33:05 +0200

    View added for project query with versions

---
+++ server/internal/db/migrations/postgres/000005_create_get_project_with_version_view.up.sql
@@ -0,0 +1,19 @@
+CREATE VIEW GET_PROJECTS_WITH_LATEST_VERSION_VIEW AS
+SELECT p.id        AS project_id,
+       p.createdat AS project_createdat,
+       p.updatedat AS project_updatedat,
+       v.id        AS version_id,
+       v.title,
+       v.description,
+       v.manager,
+       v.client,
+       v.sector,
+       v.createdat AS version_createdat,
+       v.updatedat AS version_updatedat
+FROM (SELECT v.projectid, MAX(v.createdat) AS createdAt
+      FROM version v
+      GROUP BY v.projectid
+     ) v1
+         INNER JOIN version v ON v1.projectid = v.projectid AND v1.createdAt = v.createdat
+         INNER JOIN project p ON v.projectid = p.id
+ORDER BY v.createdat DESC;
