251aef2728417b0af7f9a97009ddad341434be58
Lilahamstern <leo.ronnebro@hamsterapps.net>
2020-07-15 16:10:59 +0200

    Update of database structure removed informationt table and mergde with version

--- server/internal/db/migrations/postgres/000003_create_version_table.up.sql
+++ server/internal/db/migrations/postgres/000003_create_version_table.up.sql
@@ -1,6 +1,7 @@
-CREATE TABLE IF NOT EXISTS Information
+CREATE TABLE IF NOT EXISTS Version
 (
     id          UUID        NOT NULL     DEFAULT uuid_generate_v4(),
+    projectId   BIGINT      NOT NULL,
     orderId     BIGINT      NOT NULL,
     title       VARCHAR(40) NOT NULL,
     description VARCHAR(200),
@@ -9,5 +10,6 @@ CREATE TABLE IF NOT EXISTS Information
     sector      VARCHAR(50) NOT NULL,
     createdAt   TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
     updatedAt   TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+    FOREIGN KEY (projectId) REFERENCES Project (id) ON DELETE CASCADE,
     PRIMARY KEY (id)
 )
