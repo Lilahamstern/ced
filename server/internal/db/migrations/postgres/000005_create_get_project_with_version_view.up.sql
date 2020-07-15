36e94f0fc2619cafa3ec6338217da279ed992f11
Lilahamstern <leo.ronnebro@hamsterapps.net>
2020-07-16 01:17:43 +0200

    Added orderId to output and sorting on updatedAt instead of createdAt

--- server/internal/db/migrations/postgres/000005_create_get_project_with_version_view.up.sql
+++ server/internal/db/migrations/postgres/000005_create_get_project_with_version_view.up.sql
@@ -3,6 +3,7 @@ SELECT p.id        AS project_id,
        p.createdat AS project_createdat,
        p.updatedat AS project_updatedat,
        v.id        AS version_id,
+       v.orderid,
        v.title,
        v.description,
        v.manager,
@@ -10,10 +11,10 @@ SELECT p.id        AS project_id,
        v.sector,
        v.createdat AS version_createdat,
        v.updatedat AS version_updatedat
-FROM (SELECT v.projectid, MAX(v.createdat) AS createdAt
+FROM (SELECT v.projectid, MAX(v.updatedat) AS updatedat
       FROM version v
       GROUP BY v.projectid
      ) v1
-         INNER JOIN version v ON v1.projectid = v.projectid AND v1.createdAt = v.createdat
+         INNER JOIN version v ON v1.projectid = v.projectid AND v1.updatedat = v.updatedat
          INNER JOIN project p ON v.projectid = p.id
-ORDER BY v.createdat DESC;
+ORDER BY v.updatedat DESC;
