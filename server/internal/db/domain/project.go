14f905d2a79c46d82e565d04fdbee837c2dd2cde
Lilahamstern <leo.ronnebro@hamsterapps.net>
2020-07-17 00:45:09 +0200

    Updated output to versions insteed of version

--- server/internal/db/domain/project.go
+++ server/internal/db/domain/project.go
@@ -7,17 +7,30 @@ import (
 
 // Project : Database model
 type Project struct {
-	ID int64 `db:"project_id"`
-	Version
+	ID        int64 `db:"project_id"`
+	Versions  []Version
+	Version             // Required to scan struct from db.
 	CreatedAt time.Time `db:"project_createdat"`
 	UpdatedAt time.Time `db:"project_updatedat"`
 }
 
 // ToModel : will map database model of project to request model of project
 func (p *Project) ToModel() *model.Project {
+	versions := []model.Version{}
+	// If len(versions) is greater then 0, map values to versions
+	if len(p.Versions) != 0 {
+		for _, v := range p.Versions {
+			versions = append(versions, *v.ToModel())
+		}
+	}
+
+	if p.Version.ID != nil {
+		versions = append(versions, *p.Version.ToModel())
+	}
+
 	return &model.Project{
 		ID:        p.ID,
-		Version:   *p.Version.ToModel(),
+		Versions:  versions,
 		CreatedAt: p.CreatedAt.String(),
 		UpdatedAt: p.UpdatedAt.String(),
 	}
