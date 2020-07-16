80a84d5e4468259ee4d05c26c3be49e171d09f25
Lilahamstern <leo.ronnebro@hamsterapps.net>
2020-07-16 21:04:41 +0200

    Cleaned up database migrations

--- server/internal/db/migrations/postgres/000002_create_project_table.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS Project
-(
-    id        BIGINT NOT NULL,
-    createdAt TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
-    updatedAt TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
-    PRIMARY KEY (id)
-)
