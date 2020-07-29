52860c9aca5a7f1ffddd8ec2de9b9b32d8ed8898
Leo Rönnebro <leo.ronnebro@hamsterapps.net>
2020-07-29 13:01:26 +0200

    Added comments for easier understanding

--- server/internal/server/handler/space/space.go
+++ server/internal/server/handler/space/space.go
@@ -5,10 +5,12 @@ import (
 	"github.com/lilahamstern/ced/server/internal/repository"
 )
 
+// Handler : Space handler struct
 type Handler struct {
 	Repos *repository.Repositories
 }
 
+// Routes : Register space routes
 func (h *Handler) Routes(space *gin.RouterGroup) {
 	space.POST("/projects", h.handleProjectCreate())
 	space.GET("/projects", h.handleProjectGetAll())
