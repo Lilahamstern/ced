73142201b484715ab1725f88243124595987ab54
Leo Rönnebro <leo.ronnebro@hamsterapps.net>
2020-08-02 23:20:37 +0200

    Tooltip fix and fontawesome
    Added fontawesome
    Tooltip is now working correctly

--- web/src/components/tooltip/Tooltip.tsx
+++ web/src/components/tooltip/Tooltip.tsx
@@ -1,24 +1,47 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 interface IProps {
-  projectID: string;
+  children: React.ReactNode;
+  tooltipClassName: string;
+  iconClassName: string;
 }
 
-interface IState {}
+interface IState {
+  showTooltip: boolean;
+}
 
 export class Tooltip extends Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      showTooltip: false,
+    };
+  }
+
+  toggleTooltip = (
+    e: React.MouseEvent<SVGSVGElement, MouseEvent>,
+    show: boolean
+  ) => this.setState({ showTooltip: show });
   render() {
-    const { projectID } = this.props;
+    const { showTooltip } = this.state;
+    const { children, tooltipClassName, iconClassName } = this.props;
     return (
       <div>
-        <div>
-          <div className="bg-gray-900 opacity-75 text-white text-xs rounded py-1 px-3 right-0 bottom-full truncate">
-            {projectID}
+        {showTooltip && (
+          <div className={tooltipClassName}>
+            <div className="bg-gray-900 opacity-75 text-white text-xs rounded py-1 pl-2 right-0 bottom-full truncate">
+              {children}
+            </div>
           </div>
-        </div>
-        <div className="absolute right-0 -mt-6 -mr-3 cursor-pointer ">
+        )}
+        <div className={`absolute cursor-pointer ${iconClassName}`}>
           <p className="text-base mr-3">
-            <i className="fas fa-info-circle"></i>
+            <FontAwesomeIcon
+              onMouseEnter={(e) => this.toggleTooltip(e, true)}
+              onMouseLeave={(e) => this.toggleTooltip(e, false)}
+              icon={["fas", "info-circle"]}
+            />
           </p>
         </div>
       </div>
