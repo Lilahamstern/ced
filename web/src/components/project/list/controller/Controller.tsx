da9fae311776ae5a7cd2cb4aa230b481cf2cb862
Leo Rönnebro <leo.ronnebro@hamsterapps.net>
2020-08-02 23:21:31 +0200

    Working as expected and Fontawesome
    Everything works as expected
    added fontawesome

--- web/src/components/project/list/controller/Controller.tsx
+++ web/src/components/project/list/controller/Controller.tsx
@@ -1,26 +1,36 @@
 import React, { Component, FunctionComponent } from "react";
 
 import { ProjectViewStatus } from "../../../../enums";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconName } from "@fortawesome/fontawesome-svg-core";
 
 interface IProps {
   view: ProjectViewStatus;
+  clickHandler: (
+    event: React.MouseEvent<HTMLButtonElement>,
+    view: ProjectViewStatus
+  ) => void;
 }
 
 interface IState {}
 
 export class Controller extends Component<IProps, IState> {
   render() {
-    const { view } = this.props;
+    const { view, clickHandler } = this.props;
     return (
       <div className="bg-gray-800 text-sm text-gray-500 leading-none border-2 border-gray-800 rounded-full inline-flex">
         <Button
-          title="Card"
+          onClick={clickHandler}
+          view={ProjectViewStatus.CARD}
+          iconName="th-large"
           classes={`btn rounded-l-full focus:outline-none hover:text-gray-100 focus:text-gray-200 ${
-            view === ProjectViewStatus.CARD ? "active" : "active"
+            view === ProjectViewStatus.CARD ? "active" : ""
           }`}
         />
         <Button
-          title="List"
+          onClick={clickHandler}
+          view={ProjectViewStatus.LIST}
+          iconName="th-list"
           classes={`btn rounded-r-full focus:outline-none hover:text-white focus:text-gray-200 ${
             view === ProjectViewStatus.LIST ? "active" : ""
           }`}
@@ -34,31 +44,23 @@ export default Controller;
 
 interface ButtonProps {
   classes: string;
-  title: string;
+  view: ProjectViewStatus;
+  iconName: IconName;
+  onClick: (
+    event: React.MouseEvent<HTMLButtonElement>,
+    view: ProjectViewStatus
+  ) => void;
 }
 
 const Button: FunctionComponent<ButtonProps> = (props: ButtonProps) => {
+  const { view, classes, iconName, onClick } = props;
   return (
-    <button className="btn rounded-r-full focus:outline-none hover:text-white focus:text-gray-200">
-      <svg
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className="fill-current w-4 h-4 mr-2"
-      >
-        <line x1="8" y1="6" x2="21" y2="6"></line>
-        <line x1="8" y1="12" x2="21" y2="12"></line>
-        <line x1="8" y1="18" x2="21" y2="18"></line>
-        <line x1="3" y1="6" x2="3.01" y2="6"></line>
-        <line x1="3" y1="12" x2="3.01" y2="12"></line>
-        <line x1="3" y1="18" x2="3.01" y2="18"></line>
-      </svg>
-      <span>{props.title}</span>
+    <button className={classes} onClick={(e) => onClick(e, view)}>
+      <FontAwesomeIcon
+        icon={["fas", iconName]}
+        className="w-4 h-4 mr-2 fill-current"
+      />
+      <span className="capitalize=">{view}</span>
     </button>
   );
 };
--- web/src/components/project/search/Search.tsx
+++ web/src/components/project/search/Search.tsx
@@ -1,9 +1,23 @@
 import React, { Component } from "react";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 interface IProps {}
 
-interface IState {}
+interface IState {
+  search: string;
+}
 export class Search extends Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      search: "",
+    };
+  }
+
+  handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ search: e.target.value });
+  }
   render() {
     return (
       <div className="relative text-white max-w-xxs md:max-w-xs w-full">
@@ -11,21 +25,16 @@ export class Search extends Component<IProps, IState> {
           <input
             className="search-input"
             type="search"
-            placeholder="Search..."
+            placeholder="Search"
+            onChange={(e) => this.handleChange(e)}
+            value={this.state.search}
           />
         </label>
-        <button type="submit" className="absolute right-0 top-0 mt-3 mr-6">
-          <svg
+        <button type="submit" className="absolute right-0 top-0 mt-2 mr-6">
+          <FontAwesomeIcon
+            icon={["fas", "search"]}
             className="h-4 w-4 fill-current"
-            id="Capa_1"
-            x="0px"
-            y="0px"
-            viewBox="0 0 56.966 56.966"
-            width="512px"
-            height="512px"
-          >
-            <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
-          </svg>
+          />
         </button>
       </div>
     );
