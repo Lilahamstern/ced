f2144da61773d0970de1903448047a67f32de437
Leo Rönnebro <leo.ronnebro@hamsterapps.net>
2020-08-06 22:55:35 +0200

    Timesince
    Added timesince

--- web/src/utils/utils.ts
+++ web/src/utils/utils.ts
@@ -1,4 +1,5 @@
 import { ProjectViewStatus } from "../enums/index";
+import moment from "moment";
 
 export function widthLessThen(width: number, minWidth: number): boolean {
   return width < minWidth;
@@ -6,4 +7,9 @@ export function widthLessThen(width: number, minWidth: number): boolean {
 
 export function viewCards(status: ProjectViewStatus): boolean {
   return status === ProjectViewStatus.CARD;
+}
+
+export function getTimeSince(date: string): string {
+  let unix = Date.parse(date);
+  return moment.utc(unix).fromNow(true);
 }
