480f9bd9cd7fec3f5d88e40d242bd9b06e606596
Leo Rönnebro <leo.ronnebro@hamsterapps.net>
2020-08-06 22:55:45 +0200

    Added redux devtools remote

--- web/src/redux/store/store.tsx
+++ web/src/redux/store/store.tsx
@@ -1,16 +1,28 @@
 import { IProjectState, projectReducer } from "../reducers/ProjectReducer";
 import { combineReducers, Store, createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import { composeWithDevTools } from "remote-redux-devtools";
 
 export interface IAppState {
   projectState: IProjectState;
 }
+
+const composeEnhancers = composeWithDevTools({
+  realtime: true,
+  name: "CED Redux Devtools",
+  hostname: "localhost",
+  port: 3000,
+});
 
 const rootReducer = combineReducers<IAppState>({
   projectState: projectReducer,
 });
 
 export default function configureStore(): Store<IAppState, any> {
-  const store = createStore(rootReducer, undefined, applyMiddleware(thunk));
+  const store = createStore(
+    rootReducer,
+    undefined,
+    composeEnhancers(applyMiddleware(thunk))
+  );
   return store;
 }
