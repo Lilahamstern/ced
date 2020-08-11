e7dc3a252fc4a0881075dc8775ef926fbf347d57
Leo Rönnebro <leo.ronnebro@hamsterapps.net>
2020-08-11 23:30:15 +0200

    Optimization of container
    Trying to see if container can be optimized
    New image
    Remove redux devtools regirstation for the moment

--- web/src/redux/store/store.tsx
+++ web/src/redux/store/store.tsx
@@ -1,18 +1,17 @@
 import { IProjectState, projectReducer } from "../reducers/ProjectReducer";
 import { combineReducers, Store, createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import { composeWithDevTools } from "remote-redux-devtools";
 
 export interface IAppState {
   projectState: IProjectState;
 }
 
-const composeEnhancers = composeWithDevTools({
-  realtime: true,
-  name: "CED Redux Devtools",
-  hostname: "localhost",
-  port: 3000,
-});
+// const composeEnhancers = composeWithDevTools({
+//   realtime: true,
+//   name: "CED Redux Devtools",
+//   hostname: "localhost",
+//   port: 3000,
+// });
 
 const rootReducer = combineReducers<IAppState>({
   projectState: projectReducer,
@@ -22,7 +21,8 @@ export default function configureStore(): Store<IAppState, any> {
   const store = createStore(
     rootReducer,
     undefined,
-    composeEnhancers(applyMiddleware(thunk))
+    // composeEnhancers(applyMiddleware(thunk))
+    applyMiddleware(thunk)
   );
   return store;
 }
